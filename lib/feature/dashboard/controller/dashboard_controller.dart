f9b7835895ee155c52346eca4e3bdfa16e92f8c0
hoangankin2211 <73890707+hoangankin2211@users.noreply.github.com>
2022-11-07 13:01:49 +0700

    fix some error

--- lib/feature/dashboard/controller/dashboard_controller.dart
+++ lib/feature/dashboard/controller/dashboard_controller.dart
@@ -6,6 +6,7 @@ import 'package:ueh_project_admin/feature/home/screens/home_screen.dart';
 import 'package:ueh_project_admin/feature/dashboard/widgets/custom_left_appbar.dart';
 import 'package:ueh_project_admin/feature/employ/screens/employ_main_screen.dart';
 import 'package:ueh_project_admin/feature/mail_notification/screens/mail_notification_screen.dart';
+import 'package:ueh_project_admin/feature/messenger/screens/main_messenger_screen.dart';
 import 'package:ueh_project_admin/feature/profile/screns/profile_screen.dart';
 import 'package:ueh_project_admin/feature/task_project/screens/task_screen.dart';
 
@@ -36,11 +37,12 @@ class DashboardController extends GetxController {
       alignment: Alignment.topCenter,
       index: pageIndex.value,
       children: [
-        MailNotificationScreen(),
+        MainMessengerScreen(),
         HomeScreen(),
         EmployMainScreen(),
         TaskScreen(),
         ProfileScreen(),
+        MailNotificationScreen(),
       ],
     ),
   );
